12f7aca8e7d6607b9c36db1ac072b35d95a36b42
Andy Neale <andy@echobox.com>
2023-04-14 14:52:28 +0100

    Minor label change

--- src/Export.jsx
+++ src/Export.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Export = ({ filename, onChange, onExport, exportType }) => (
   <div>
-    <label htmlFor="filename">Filename</label>
+    <label htmlFor="filename">Filename:</label>
     <input name="filename" type="text" value={filename} onChange={onChange} />
     <label>.vtl</label>
     <button onClick={onExport}>Export {exportType}</button>
