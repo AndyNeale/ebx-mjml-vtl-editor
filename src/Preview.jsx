e116ad4c49c0e0d02d091d0d27be271a1504b724
Andy Neale <andy@echobox.com>
2023-04-18 16:00:37 +0100

    Added the ability to see generated HTML in the preview panel

--- src/Preview.jsx
+++ src/Preview.jsx
@@ -1,19 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+import pretty from "pretty";
 
-const Preview = ({ html }) => (
-  <div>
-    <div className="section-titles">Preview</div>
+const Preview = ({ html }) => {
+  const [currentTab, setCurrentTab] = useState("Preview");
+
+  return (
     <div>
-      <iframe
-        className="preview"
-        id="previewIframe"
-        loading="lazy"
-        srcDoc={html}
-        title="Edition Preview"
-        allowFullScreen
-      />
+      <div className="section-titles">
+        <a href="#" onClick={() => setCurrentTab("Preview")}>
+          Preview
+        </a>{" "}
+        |{" "}
+        <a href="#" onClick={() => setCurrentTab("HTML")}>
+          HTML
+        </a>
+      </div>
+      <div>
+        {currentTab === "Preview" ? (
+          <iframe
+            className="preview"
+            id="previewIframe"
+            loading="lazy"
+            srcDoc={html}
+            title="Edition Preview"
+            allowFullScreen
+          />
+        ) : (
+          <div className="preview html">
+            <pre>{pretty(html)}</pre>
+          </div>
+        )}
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Preview;
