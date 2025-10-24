6359a97b4cc08a4395e66cec95fa8810d7a8f1db
AndyNeale <andy@andyneale.com>
2025-10-24 15:40:28 +0100

    Add desktop-mobile toggle

--- src/Preview.jsx
+++ src/Preview.jsx
@@ -3,6 +3,7 @@ import pretty from "pretty";
 
 const Preview = ({ html }) => {
   const [currentTab, setCurrentTab] = useState("Preview");
+  const [isDesktop, setDesktop] = useState(true);
 
   const copyHTMLToClipboard = () => {
     if (navigator.clipboard) {
@@ -19,28 +20,37 @@ const Preview = ({ html }) => {
       <div className="section-titles-container">
         <div className="d-flex">
           <div className="section-titles">
-            <button onClick={() => setCurrentTab("Preview")}>Preview</button>
+            <button onClick={() => setCurrentTab("Preview")} className={currentTab === "Preview" ? "active" : ""}>
+              Preview
+            </button>
           </div>
           <div className="section-titles">
-            <button onClick={() => setCurrentTab("HTML")}>HTML</button>
+            <button onClick={() => setCurrentTab("HTML")} className={currentTab === "HTML" ? "active" : ""}>
+              HTML
+            </button>
           </div>
           {currentTab === "HTML" && (
             <div className="section-titles">
               <button onClick={copyHTMLToClipboard}>Copy</button>
             </div>
           )}
+          <div className="section-titles">
+            <button onClick={() => setDesktop(true)} className={isDesktop ? "active" : ""}>
+              Desktop
+            </button>
+          </div>
+          <div className="section-titles">
+            <button onClick={() => setDesktop(false)} className={isDesktop ? "" : "active"}>
+              Mobile
+            </button>
+          </div>
         </div>
       </div>
       <div>
         {currentTab === "Preview" ? (
-          <iframe
-            className="preview"
-            id="previewIframe"
-            loading="lazy"
-            srcDoc={html}
-            title="Edition Preview"
-            allowFullScreen
-          />
+          <div className="preview">
+            <iframe className={isDesktop ? "desktop" : "mobile"} id="previewIframe" loading="lazy" srcDoc={html} title="Edition Preview" allowFullScreen />
+          </div>
         ) : (
           <div className="preview html">
             <pre>{pretty(html)}</pre>
