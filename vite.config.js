124740095986b5d03a783e17bf3963dae944cbf0
AndyNeale <andy@andyneale.com>
2025-10-27 08:20:32 +0000

    Updated packages

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ebx-mjml-vtl-editor/',
+  base: "/",
   build: {
-    outDir: 'build',
+    outDir: "build",
   },
   server: {
     port: 3000,
-    open: true
-  }
-})
+    open: true,
+  },
+});
